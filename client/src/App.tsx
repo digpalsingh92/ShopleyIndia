4b61be1f4e0bccb112273c31f42b37814eca319d
Digpal Singh <digpalsingh18901@gmail.com>
2025-01-02 23:50:42 +0530

    fake commit message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
+import { Routes, Route } from "react-router"
+import Home from "./pages/Home"
 
 export default function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
